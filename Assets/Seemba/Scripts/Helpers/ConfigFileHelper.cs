ebbd614cc0a09f91c1eeaea721c062a2b964758b
Bachar bhouri <bacharbhouri@gmail.com>
2020-10-16 16:38:58 +0100

    update dependencies

--- Assets/Seemba/Scripts/Helpers/ConfigFileHelper.cs
+++ Assets/Seemba/Scripts/Helpers/ConfigFileHelper.cs
@@ -2,11 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ConfigFileHelper : MonoBehaviour {
+public class ConfigFileHelper : MonoBehaviour
 {
-    private const string CONFIG_FILE_NAME = "seemba-services";
-    private static string PATH = Application.dataPath + "/Seemba/Resources/";
-    private static string RELATIVE_PATH = "Assets/Seemba/Resources/";
+    public const string CONFIG_FILE_NAME = "seemba-services";
+    public static string PATH = Application.dataPath + "/Seemba/Resources/";
+    public static string RELATIVE_PATH = "Assets/Seemba/Resources/";
     public  static string FILE_PATH = PATH + CONFIG_FILE_NAME + ".json";
     public  static string RELATIVE_FILE_PATH = RELATIVE_PATH + CONFIG_FILE_NAME + ".json";
 }

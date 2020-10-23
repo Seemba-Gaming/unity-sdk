9647d1ddacb0f9d077f9f9af5b95c07fdb30538a
Bachar bhouri <bacharbhouri@gmail.com>
2020-10-23 17:22:20 +0100

    commenting NotificationController.cs

--- Seemba/Scripts/Controller/NotificationController.cs
+++ Seemba/Scripts/Controller/NotificationController.cs
@@ -1,25 +1,25 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using System;
+﻿using UnityEngine;
 public class NotificationController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-    Firebase.Messaging.FirebaseMessaging.TokenReceived += OnTokenReceived; 
-    Firebase.Messaging.FirebaseMessaging.MessageReceived += OnMessageReceived;
-}
-public void OnTokenReceived(object sender, Firebase.Messaging.TokenReceivedEventArgs token) {
-  UnityEngine.Debug.Log("Received Registration Token: " + token.Token);
-  PlayerPrefs.SetString("DeviceToken",token.Token);
-}
-public void OnMessageReceived(object sender, Firebase.Messaging.MessageReceivedEventArgs e) {
-  UnityEngine.Debug.Log("Received a new message from: " + e.Message.From);
-  //Assets.SimpleAndroidNotifications.NotificationManager.SendWithAppIcon(TimeSpan.FromSeconds(0), e.Message.Notification.Title, e.Message.Notification.Body, new Color(0, 0.6f, 1), Assets.SimpleAndroidNotifications.NotificationIcon.Event);
-}
-    // Update is called once per frame
-    void Update()
-    {
-    }
+    //// Start is called before the first frame update
+    //private void Start()
+    //{
+    //    Firebase.Messaging.FirebaseMessaging.TokenReceived += OnTokenReceived;
+    //    Firebase.Messaging.FirebaseMessaging.MessageReceived += OnMessageReceived;
+    //}
+    //public void OnTokenReceived(object sender, Firebase.Messaging.TokenReceivedEventArgs token)
+    //{
+    //    UnityEngine.Debug.Log("Received Registration Token: " + token.Token);
+    //    PlayerPrefs.SetString("DeviceToken", token.Token);
+    //}
+    //public void OnMessageReceived(object sender, Firebase.Messaging.MessageReceivedEventArgs e)
+    //{
+    //    UnityEngine.Debug.Log("Received a new message from: " + e.Message.From);
+    //    //Assets.SimpleAndroidNotifications.NotificationManager.SendWithAppIcon(TimeSpan.FromSeconds(0), e.Message.Notification.Title, e.Message.Notification.Body, new Color(0, 0.6f, 1), Assets.SimpleAndroidNotifications.NotificationIcon.Event);
+    //}
+
+    //// Update is called once per frame
+    //private void Update()
+    //{
+    //}
 }

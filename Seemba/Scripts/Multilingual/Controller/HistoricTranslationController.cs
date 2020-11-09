cb7b351b7f1e1047038eeb18feadd9b1b21c5ee4
Bachar bhouri <bacharbhouri@gmail.com>
2020-11-09 11:57:44 +0100

    back button refactor & translation

--- Seemba/Scripts/Multilingual/Controller/HistoricTranslationController.cs
+++ Seemba/Scripts/Multilingual/Controller/HistoricTranslationController.cs
@@ -8,10 +8,12 @@ public class HistoricTranslationController : MonoBehaviour
 {
     [SerializeField]
     private Text games_won,
-                 in_a_row;
+                 in_a_row, BackButton;
     // Start is called before the first frame update
     void Start()
     {
+        TranslationManager.scene = "Home";
+        BackButton.text = TranslationManager.Get("back_button");
         TranslationManager.scene = "History";
         games_won.text = TranslationManager.Get("games_won") != string.Empty ? TranslationManager.Get("games_won") : games_won.text;
         in_a_row.text = TranslationManager.Get("in_a_row") != string.Empty ? TranslationManager.Get("in_a_row") : in_a_row.text;
